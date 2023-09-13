8813f831aa2449e123bc92d5c18e03fbe94f7df9
speedshi <speedshi@hotmail.com>
2023-09-13 17:24:41 +0200

    update

--- usage_example.py
+++ usage_example.py
@@ -13,5 +13,7 @@ stream.filter('bandpass', freqmin=2, freqmax=40)
 stream.plot()
 output = qkp_apply.qkphase(stream, file_para='./parameters.yaml')
 
+for ipick in output.picks:
+    print(ipick)
 
 
