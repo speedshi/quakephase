9a52e699ec38fada1857b55b4f92a401b192b815
speedshi <speedshi@hotmail.com>
2023-11-09 15:38:02 +0100

    update xpick

--- quakephase/xpick.py
+++ quakephase/xpick.py
@@ -44,6 +44,8 @@ def get_picks(prob, paras):
                                        rel_height=paras['pick']['rel_height'], 
                                        plateau_size=paras['pick']['plateau_size'])
             trace_id = f"{iprob.stats.network}.{iprob.stats.station}.{iprob.stats.location}"
+            kflag = 0  # array index
+            Nsamp = np.size(iprob.data)  # total number of sample in probability curve
             for kk, kpki in enumerate(peaks_indx):
                 if paras['pick'][f"{itag}_threshold"] is None:
                     kk_start_time = iprob.stats.starttime + iprob.times()[kpki]
@@ -52,17 +54,54 @@ def get_picks(prob, paras):
                     kk_peak_value = iprob.data[kpki]
                 else:
                     # have input threshold
+                    if (kpki < kflag): continue
                     assert(iprob.data[kpki]>=paras['pick'][f"{itag}_threshold"])
-                    ### TO DO ...
-                    kk_start_time = 
-                    kk_end_time = 
-                    kk_peak_time = 
-                    kk_peak_value = 
+
+                    fgia = np.zeros(Nsamp,dtype=bool)
+                    fgia[kflag:] = True
+                    usia = (iprob.data >= paras['pick'][f"{itag}_threshold"]) & (fgia)
+                    if sum(usia)==0: break
+                    start_idx = np.argmax(usia)
+                    assert(iprob.data[start_idx] >= paras['pick'][f"{itag}_threshold"])
+
+                    fgia_e = np.zeros(Nsamp,dtype=bool)
+                    fgia_e[start_idx:] = True
+                    usia_e = (iprob.data < paras['pick'][f"{itag}_threshold"]) & (fgia_e)
+                    if sum(usia_e)==0: 
+                        end_idx = Nsamp - 1
+                    else:
+                        end_idx = np.argmax(usia_e) - 1
+                    assert(iprob.data[end_idx] >= paras['pick'][f"{itag}_threshold"])
+
+                    if kpki > end_idx:
+                        kflag = end_idx + 1
+                        if kflag == Nsamp:
+                            break
+                        else:
+                            continue
+                    else:
+                        assert(kpki>=start_idx)
+                        peak_idx = kpki
+                        for mm in range(kk+1, len(peaks_indx)):
+                            if peaks_indx[mm] > end_idx:
+                                break
+                            else:
+                                if iprob.data[peaks_indx[mm]] > iprob.data[peak_idx]:
+                                    peak_idx = peaks_indx[mm]
+                    assert(iprob.data[peak_idx] >= paras['pick'][f"{itag}_threshold"])
+
+                    assert(start_idx <= peak_idx <= end_idx)
+                    kk_start_time = iprob.stats.starttime + iprob.times()[start_idx]
+                    kk_end_time = iprob.stats.starttime + iprob.times()[end_idx]
+                    kk_peak_time = iprob.stats.starttime + iprob.times()[peak_idx]
+                    kk_peak_value = iprob.data[peak_idx]
+                    kflag = end_idx + 1
+                    if kflag == Nsamp: break
                 kkpick = sbPick(trace_id=trace_id, start_time=kk_start_time, end_time=kk_end_time,
                                 peak_time=kk_peak_time, peak_value=kk_peak_value, phase=itag)
                 picks += [kkpick]
         else:
-            raise ValueError
+            raise ValueError(f"Invalid input for pick method: {paras['pick']['method']}!")
 
     return sbu.PickList(sorted(picks))
 
