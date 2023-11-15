1a1abe908f0105d6368b25ba5120ba35f3502cbf
speedshi <speedshi@hotmail.com>
2023-11-15 18:23:08 +0100

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='quakephase',
-    version='0.2.0',
+    version='0.2.2',
     packages=find_packages(),
     install_requires=[
         'seisbench',
