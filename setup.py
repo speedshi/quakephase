f13b6cbdfb8843b4bdf83bfe3199674cc61ca1c2
Peidong Shi <speedshi@hotmail.com>
2024-02-15 22:54:35 +0100

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='quakephase',
-    version='0.2.2',
+    version='0.2.4',
     packages=find_packages(),
     author='Peidong Shi',
     author_email="speedshi@hotmail.com",
