501a9f97ea417b3933cf237f36924ae24d234fad
Peidong Shi <speedshi@hotmail.com>
2024-02-16 07:38:30 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='quakephase',
@@ -7,6 +11,8 @@ setup(
     author='Peidong Shi',
     author_email="speedshi@hotmail.com",
     description="A Python package for automatic seismic phase characterization",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/speedshi/quakephase",
     license="GPLv3",
     keywords="Seismology, Machine Learning, Phase Picking, Seismic Monitoring, Earthquake Monitoring",
